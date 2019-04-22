6c79a35bc7c1ad7f989b9bfe3ca848e9ae6d939d
hoanqak <hoanqak@users.noreply.github.com>
2019-04-22 15:25:48 +0700

    add new file sql

---
+++ SellPhoneProject/MYSQL.sql
@@ -0,0 +1,32 @@
+create database sellphone;
+use sellphone;
+
+create table category(
+	id int primary key auto_increment,
+    category nvarchar(40) not null
+);
+
+create table product(
+    numberProduct int auto_increment primary key,
+    name nvarchar(40) not null,
+    price double,
+    description nvarchar(100),
+    discount double default 0,
+    number_of_product int,
+    category_id int not null,
+    image varchar(200) default 'default.jpg',
+    foreign key(category_id) references category(id)
+);
+alter table product add column ram varchar(6);
+alter table product add column memory varchar(6);
+alter table product add column chip varchar(30);
+alter table product add column camera nvarchar(50);
+
+
+create table listImage(
+	id int primary key auto_increment,
+    id_product int not null,
+    image varchar(500) not null
+);
+
+select * from product as p order by p.price desc limit 0, 8
